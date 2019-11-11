babfb14736da5454b340c80a2e76b52d676b3d70
Mirino97 <j.cesarueti@yahoo.com.br>
2019-11-11 14:58:27 -0300

    Fucking with the layout a little bit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,38 +1,46 @@
 @extends('layout')
 
 @section('content')
-
-<div class="createForm">
-	<form method="post" action="/create" class="container pt-3 pb-3">
+	<form method="post" action="/create" style="width: 90%; height: 90%; border: solid .2em #f7f7f9;">
 
 		@csrf
+		<div class="form-row justify-content-center">
+			<h4>Cadastrar Novo Cliente</h4>
+		</div>
+		<div class="form-row justify-content-center">
+				<div class="form-group col-md-4">
+					<label for="inputNome">Nome</label>
+					<input type="text" name="nome" class="form-control" placeholder="Cliente" required>
+				</div>
 
-		<h4>Cadastrar Novo Cliente</h4>
-		<div class="form-group">
-			<input type="text" name="nome" placeholder="Cliente" required>
+				<div class="form-group">
+					<label for="inputTelefone">Telefone</label>
+					<input type="text" class="form-control" name="telefone" placeholder="Telefone" required>
+				</div>
+
+				<div class="form-group col-md-3">
+					<label for="inputEmail">Email</label>
+					<input type="text" name="email" class="form-control" placeholder="E-mail" required>
+				</div>
 		</div>
 
-		<div class="form-group">
-			<input type="text" name="telefone" placeholder="Telefone" required>
+		<div class="form-row justify-content-center">
+			<div class="form-group col-md-5">
+				<label for="inputEndereco">Endereço</label>
+				<input type="text" name="endereco" class="form-control" placeholder="Endereço" required>
+			</div>
+
+			<div class="form-group col-md-4">
+				<label for="inputCNPJ">CNPJ</label>
+				<input type="text" name="cnpj" class="form-control" placeholder="CNPJ" required>
+			</div>
+		</div>
+		<div class="form-row justify-content-center">
+			<input type="submit" name="button" value="Cadastrar" class="btn bg-success" style="color: white">
 		</div>
 
-		<div class="form-group">
-			<input type="text" name="endereco" placeholder="Endereço" required>
-		</div>
-
-		<div class="form-group">
-			<input type="text" name="email" placeholder="E-mail" required>
-		</div>
-
-		<div class="form-group">
-			<input type="text" name="cnpj" placeholder="CNPJ" required>
-		</div>
-
-		<input type="submit" name="button" value="Cadastrar" class="btn bg-success" style="color: white">
 
 	</form>
-</div>
-
 @endsection
 
 @section('lista')
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,35 +6,49 @@
 </head>
 <body>
 
-    <div id="navbar">
-        <div id="navbar-logo">
-            <p>LOGO</p>
+<div class="container-fluid">
+    <div class="row" style="height: 50px;">
+        <div class="col" style="background-color: red;">.col</div>
+    </div>
+    <div class="row">
+        <div class="row" style="height: 93.7vh; width: 200px;">
+            <div class="col" style="background-color: purple; height: 100%">
+                <div class="container-fluid" style="height: 100%; width: 100%; padding: 0px;">
+                    <div class="col">
+                        <div class="row">
+                            <a href="" class="navBarButton">TESTE</a>
+                        </div>
+                        <div class="row">
+                            <a href="" class="navBarButton">TESTE</a>
+                        </div>
+                        <div class="row">
+                            <a href="" class="navBarButton">TESTE</a>
+                        </div>
+                        <div class="row">
+                            <a href="" class="navBarButton">TESTE</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div id="filler"></div>
-        <a href="" class="button">
-            Clientes
-        </a>
-        <a href="" class="button">
-            Serviços
-        </a>
-        <a href="" class="button">
-            Financeiro
-        </a>
+        <div class="col">
+            <div class="row justify-content-center" style="margin: 0px"> 
+                @yield('content')
+            </div>
+
+            <div class="row justify-content-center" style="margin: 0px">
+                @yield('lista')
+            </div>
+            @yield('ordens')
+            @foreach ($errors->all() as $error)
+            <div class="alert alert-danger">{{ $error }}</div>
+            @endforeach
+            @yield('listaOrdens')
+            @yield('editarOrdem')
+        </div>
     </div>
-
-    <div id="container">
-
-	   @yield('content')
-       @yield('lista')
-       @yield('ordens')
-
-    </div>
-    @foreach ($errors->all() as $error)
-        <div class="alert alert-danger">{{ $error }}</div>
-    @endforeach
-    @yield('listaOrdens')
-    @yield('editarOrdem')
-
+     
+</div>
 
 </body>
 </html>
