ba2d8c6cd89a74c5906267d76196653941c34dd2
Marnei Maximiano <marneimax@gmail.com>
2019-11-12 23:37:17 -0300

    update

--- resources/views/editarOrdem.blade.php
+++ resources/views/editarOrdem.blade.php
@@ -1,30 +1,28 @@
 @extends('layout')
-
 @section('editarOrdem')
-
-<div class="formOrdem">
-	<form method="post" action="/{{$ordem->id}}/ordemUpdate" id="formOrdem1" >
-		@csrf
-		@method('PATCH')
- 		<h4>Editar Ordem de Serviço</h4>
-			<input type="hidden" name="ordem_id" value="{{$ordem->id}}">
-			<input type="text" name="servico" placeholder="Serviço Prestado" value="{{$ordem->servico}}" required>
-
-			<input type="date" name="dataServico" placeholder="Data de Serviço" value="{{$ordem->dataServico}}" required>
-
-			<input type="text" name="valor" placeholder="valor" value="{{$ordem->valor}}" required>
-
-			<select name="pago">
-			    <option value="nao" {{($ordem['pago']=='nao'?'selected':'')}}>Não</option>
-			    <option value="sim" {{($ordem['pago']=='sim'?'selected':'')}}>Sim</option>
-  			</select>
-
-			<textarea name="observacao" placeholder="Descrição do Serviço">{{$ordem->observacao}}</textarea>
-
-		<input type="submit" name="button" value="Atualizar" class="btn bg-success" style="color: white">
-
-	</form>
-</div>
-
-
+<form method="post" action="/{{$ordem->id}}/ordemUpdate">
+	@csrf
+	@method('PATCH')
+	<h4>Editar Ordem de Serviço</h4>
+	<input class="w-100 rounded p-2 mb-2" type="hidden" name="ordem_id" value="{{$ordem->id}}">
+	<div class="row">
+		<div class="col-12"><input class="w-100 rounded p-2 mb-2" type="text" name="servico" placeholder="Serviço Prestado" value="{{$ordem->servico}}" required></div>
+	</div>
+	<div class="row">
+		<div class="col-4"><input class="w-100 rounded p-2 mb-2" type="date" name="dataServico" placeholder="Data de Serviço" value="{{$ordem->dataServico}}" required></div>
+		<div class="col-4"><input class="w-100 rounded p-2 mb-2" type="text" name="valor" placeholder="valor" value="{{$ordem->valor}}" required></div>
+		<div class="col-4">
+			<select class="w-100 rounded p-2 mb-2" name="pago">
+				<option value="nao" {{($ordem['pago']=='nao'?'selected':'')}}>Não</option>
+				<option value="sim" {{($ordem['pago']=='sim'?'selected':'')}}>Sim</option>
+			</select>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12"><textarea class="w-100 rounded p-2 mb-2" name="observacao" placeholder="Descrição do Serviço">{{$ordem->observacao}}</textarea></div>
+	</div>
+	<div class="row justify-content-center">
+		<div class="col-4"><input class="w-100 bg-success border-0 rounded p-2 mb-2" type="submit" name="button" value="Atualizar" class="btn bg-success" style="color: white"></div>
+	</div>
+</form>
 @endsection
