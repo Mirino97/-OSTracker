9d00d534ddb33e19bebd6c48f1a9770ad149b195
Mirino97 <j.cesarueti@yahoo.com.br>
2019-11-20 11:51:36 -0300

    bugfix

--- app/Http/Controllers/OrdensController.php
+++ app/Http/Controllers/OrdensController.php
@@ -7,6 +7,10 @@ use App\ordens;
 
 class OrdensController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function store(ordens $ordens)
     {
